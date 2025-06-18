d86595745e9af455ff0e8ced60841e634d6fae31
Youssifahmed12 <youssefbro12397@gmail.com>
2025-06-19 02:27:10 +0300

    Try Catch Blocks

---
+++ TryCatch.sql
@@ -0,0 +1,12 @@
+BEGIN TRY
+
+    INSERT INTO Employees3 (EmployeeID, Name, Position) VALUES (1, 'John Doe', 'Sales Manager');
+    
+    -- Attempt to insert a duplicate record which will cause an error
+    INSERT INTO Employees3 (EmployeeID, Name, Position) VALUES (1, 'Jane Smith', 'Marketing Manager');
+END TRY
+BEGIN CATCH
+
+    PRINT 'An error occurred: ' + ERROR_MESSAGE();
+
+END CATCH
